72210ceaca2b9fd0f3524772dc59a67eaf8eb402
wandersoncop <wandersoncop@gmail.com>
2024-04-01 08:16:23 -0300

    atualização

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
-continuar_usando = "SIM"
+continua = "SIM"
 
-while continuar_usando == "SIM":
+while continua == "SIM":
   
   print("SELECIONE A OPERAÇÃO DESEJADA")
   print("1: Soma")
   print("2: Subtração")
   print("3: Multiplicação")
   print("4: Divisão")
+  print("0: Para sair")
 
   
   operacao = (input("\nQual operação você deseja realizar? "))
@@ -20,38 +21,39 @@ while continuar_usando == "SIM":
     print(adicao)
     print("*"*33,"\n")
     
-    continuar_usando = input("Gostaria de fazer outra operação? ").upper()
+    continua = input("Gostaria de fazer outra operação? ").upper()
     print("*"*33,"\n")
 
-  #Subtração
+ 
   if operacao == "2":
     b1 = float(input("\nDigite o primeiro valor: "))
     b2 = float(input("Digite o segundo valor: "))
     subtracao = b1 - b2
     print("\nA subtração entre",b1,"e",b2,"é:",subtracao,"\n")
     print("*"*33,"\n")
-    continuar_usando = input("Gostaria de fazer outra operação? ").upper()
+    continua = input("Gostaria de fazer outra operação? ").upper()
     print("*"*33,"\n")
 
-  #Multiplicação
+
   if operacao == "3":
     c1 = float(input("\nDigite o primeiro valor: "))
     c2 = float(input("Digite o segundo valor: "))
     multiplicacao = c1 * c2
     print("\nA multiplicação entre",c1,"e",c2,"é:",multiplicacao,"\n")
     print("*"*33,"\n")
-    continuar_usando = input("Gostaria de fazer outra operação? ").upper()
+    continua = input("Gostaria de fazer outra operação? ").upper()
     print("*"*33,"\n")
 
-  #Divisão
+ 
   if operacao == "4":
     d1 = float(input("\nDigite o primeiro valor: "))
     d2 = float(input("Digite o segundo valor: "))
-    while d2 == 0:                  #Garantindo que d2 não seja zero!!
+    while d2 == 0:                  
+
       print("O segundo valor não pode ser zero!")
       d2 = float(input("\nDigite o segundo valor (diferente de zero): "))
     divisao = d1 / d2
     print("\nA divisão entre",d1,"e",d2,"é:",divisao,"\n")
     print("*"*33,"\n")
-    continuar_usando = input("Gostaria de fazer outra operação? ").upper()
+    continua = input("Gostaria de fazer outra operação? ").upper()
     print("*"*33,"\n")
